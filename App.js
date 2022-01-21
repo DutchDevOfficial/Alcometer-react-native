22c6ae94506f5fc2a9edcd7dcee64bcef27c5b3a
Wimme <wimmedg@gmail.com>
2022-01-21 17:39:37 +0200

    WORKING picker

--- App.js
+++ App.js
@@ -5,14 +5,11 @@ import React, {useState} from 'react';
 
 export default function App() {
 
-  const [selectedLanguage, setSelectedLanguage] = useState("TEST");
-
-
-  const [weight, setWeight] = useState(0)
-  const [bottles, setBottles] = useState(1)
+   const [weight, setWeight] = useState()
+  const [bottles, setBottles] = useState()
   const [time, setTime] = useState(1)
   const [gender, setGender] = useState("male")
-  const [result, setResult] = useState(0)
+  const [result, setResult] = useState(0.00)
 
   function calculate() {
     const grams = ((bottles * 0.33) * 8 * 4.5) - ((weight / 10) * time)
@@ -32,13 +29,19 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Alcometer</Text>
+
+      <Text style={styles.text}>Weight</Text>
+      <TextInput style={styles.field} value={weight} onChangeText={text => setWeight(text)} />
+
 
       <Text style={styles.text}>Bottles</Text>
       <Picker
+        style={styles.picker}
         selectedValue={bottles}
-        onValueChange={(itemValue, itemIndex) =>
-          setBottles(itemValue)
+        mode="dropdown"
+        onValueChange={(value, itemIndex) =>
+          setBottles(value)
         }>
         <Picker.Item label="1 bottle" value="1" />
         <Picker.Item label="2 bottles" value="2" />
@@ -51,7 +54,9 @@ export default function App() {
       <Text style={styles.text}>Time</Text>
       <Picker
         selectedValue={time}
-        onValueChange={(itemValue, itemIndex) =>
+        mode="dropdown"
+        style={styles.picker}
+        onValueChange={(itemValue) =>
           setTime(itemValue)
         }>
         <Picker.Item label="1 hour" value="1" />
@@ -61,8 +66,10 @@ export default function App() {
         <Picker.Item label="5 hours" value="5" />
       </Picker>
 
+      <Text style={styles.text}>Gender</Text>
 
-      <Text style={styles.result}>{result}</Text>
+
+      <Text style={styles.result}>{result.toFixed(2)}</Text>
 
       <Button title='CALCULATE' onPress={calculate}></Button>
     </View>
@@ -79,9 +86,29 @@ const styles = StyleSheet.create({
   result: {
     color:'yellow',
     marginBottom: 10,
-    marginTop:10
+    marginTop:10,
+    fontSize:40
+  },
+  title:{
+    color: 'blue',
+    fontSize:40,
+    marginBottom:20
+  },
+  text: {
+    textAlign:'left',
+    fontStyle:'normal',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontWeight:"bold",
+    width: 300,
   },
   picker: {
-
+    marginVertical: 10,
+    width: 300,
+    borderWidth: 1,
+    borderColor: "#666",
+  },
+  field:{
+    width: 300,
   }
 });
