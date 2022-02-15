fe6e8bc06358951ef4feef9a468c936dc60a0403
Wimme <wimmedg@gmail.com>
2022-02-15 14:33:33 +0200

    EDIT views

--- components/alcometer.js
+++ components/alcometer.js
@@ -4,6 +4,7 @@ import {Picker} from '@react-native-picker/picker';
 import React, {useState} from 'react';
 
 
+
 export default function Alcometer() {
 
   const [weight, setWeight] = useState("");
@@ -15,17 +16,25 @@ export default function Alcometer() {
   const [resultColor, setResultColor] = useState("grey");
 
   var radio_props = [
-    {label: 'Male', value: 0 },
-    {label: 'Female', value: 1 }
+    {label: 'Male', value: 'male' },
+    {label: 'Female', value: 'female' }
   ];
 
   function calculate() {
+
+    const NoWeightAlert = () =>
+    Alert.alert(
+      "No Weight",
+      "No Weight has been filled in",
+    );
+
     const grams = ((bottles * 0.33) * 8 * 4.5) - ((weight / 10) * time);
     let result = null;
 
     // alert
     if(weight === ""){
       NoWeightAlert();
+      return;
     }
 
     if(gender === "male") {
@@ -43,11 +52,7 @@ export default function Alcometer() {
     setResultColor(GetResultColor(result));
   }
 
-  const NoWeightAlert = () =>
-  Alert.alert(
-    "No Weight",
-    "No Weight has been filled in",
-  );
+
 
   const GetResultColor = (input) => {
     let color;
@@ -67,50 +72,57 @@ export default function Alcometer() {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollView}>
-      <Text style={styles.title}>Alcometer</Text>
 
-      <Text style={styles.text}>Weight</Text>
-      <TextInput style={styles.field} value={weight} onChangeText={text => setWeight(text)} keyboardType="numeric"/>
+      {/* HEADER */}
+      <View style={styles.header}>
+        <Text style={styles.title}>Alcometer</Text>
+      </View>
+      
+      <View>
+        <Text style={styles.text}>Weight</Text>
+        <TextInput style={styles.field} value={weight.toString()} onChangeText={text => setWeight(text)} keyboardType="numeric" placeholder='Enter your weight'/>
+      </View>
 
+      <View>
+        <Text style={styles.text}>Bottles</Text>
+        <Picker
+            style={styles.picker}
+            selectedValue={bottles}
+            mode="dropdown"
+            onValueChange={(value, itemIndex) =>
+            setBottles(value)
+            }>
+            <Picker.Item label="1 bottle" value="1" />
+            <Picker.Item label="2 bottles" value="2" />
+            <Picker.Item label="3 bottles" value="3" />
+            <Picker.Item label="4 bottles" value="4" />
+            <Picker.Item label="5 bottles" value="5" />
+        </Picker>
+      </View>
 
-      <Text style={styles.text}>Bottles</Text>
-      <Picker
-        style={styles.picker}
-        selectedValue={bottles}
-        mode="dropdown"
-        onValueChange={(value, itemIndex) =>
-          setBottles(value)
-        }>
-        <Picker.Item label="1 bottle" value="1" />
-        <Picker.Item label="2 bottles" value="2" />
-        <Picker.Item label="3 bottles" value="3" />
-        <Picker.Item label="4 bottles" value="4" />
-        <Picker.Item label="5 bottles" value="5" />
-      </Picker>
-
-
-      <Text style={styles.text}>Time</Text>
-      <Picker
-        selectedValue={time}
-        mode="dropdown"
-        style={styles.picker}
-        onValueChange={(itemValue) =>
-          setTime(itemValue)
-        }>
-        <Picker.Item label="1 hour" value="1" />
-        <Picker.Item label="2 hours" value="2" />
-        <Picker.Item label="3 hours" value="3" />
-        <Picker.Item label="4 hours" value="4" />
-        <Picker.Item label="5 hours" value="5" />
-      </Picker>
-
+      <View>
+        <Text style={styles.text}>Time</Text>
+        <Picker
+            selectedValue={time}
+            mode="dropdown"
+            style={styles.picker}
+            onValueChange={(itemValue) =>
+            setTime(itemValue)
+            }>
+            <Picker.Item label="1 hour" value="1" />
+            <Picker.Item label="2 hours" value="2" />
+            <Picker.Item label="3 hours" value="3" />
+            <Picker.Item label="4 hours" value="4" />
+            <Picker.Item label="5 hours" value="5" />
+        </Picker>
+      </View>
 
 
       <Text style={styles.text}>Gender</Text>
       <RadioForm
         radio_props={radio_props}
         initial={0}
-        onPress={(value) => {this.setState({value:value})}}
+        onPress={(value) => {setGender(value)}}
       />
 
 
@@ -120,8 +132,10 @@ export default function Alcometer() {
       <Button title='CALCULATE' onPress={calculate}></Button>
 
 
-
-
+      {/* FOOTER */}
+      <View style={styles.footer}>
+        <Text style={styles.author}>Author: Wimme De Gendt</Text>  
+      </View>  
 
 
       </ScrollView>
